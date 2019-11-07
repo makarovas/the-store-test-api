e9d7267cdad3399a63a8b086c7631b16c2e3987e
Alexander Makarof <makarofalex@gmail.com>
2019-11-07 21:07:32 +0300

    init proj

---
+++ src/services/bookstore-service.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class BookStoreService extends Component {
+	render() {
+		return (
+			<div>
+				BookStoreService
+			</div>
+		)
+	}
+}
