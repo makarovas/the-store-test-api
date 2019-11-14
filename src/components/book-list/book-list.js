72f8d2713d60187fde981e0862c8f8612ee14ff8
Alexander Makarof <makarofalex@gmail.com>
2019-11-14 21:38:07 +0300

    feat(book-list): add mapstatetoprops to component

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -1,22 +1,34 @@
-import React, {Component} from 'react';
-import {BookListItem} from '../book-list-item';
+import React, { Component } from 'react';
+import { BookListItem } from '../book-list-item';
 import styled from 'styled-components';
-
+import { connect } from 'react-redux';
+import { withBookService } from '../hoc';
 class BookList extends Component {
     componentDidMount() {
+        // 1 receive data
+        const { bookstoreService } = this.props;
+        const data = bookstoreService.getBooks();
+
+        // 2 dispatch
     }
 
     render() {
-        const {books} = this.props;
+        const { books } = this.props;
 
         return (
             <ul>
-                {books.map((book)=>{
-                    return (<li key={book.id}>{<BookListItem book={book}/>}</li>)
+                {books.map((book) => {
+                    return (
+                        <li key={book.id}>{
+                            <BookListItem book={book} />}
+                        </li>)
                 })}
             </ul>
         );
     }
 }
 
-export  {BookList};
+const mapStateToProps = ({ books }) => {
+    return { books }
+}
+export default withBookService()(connect(mapStateToProps)(BookList));
